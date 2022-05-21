dbe19a834172b3a7068c7bd8c0bb3c5d5ddfe628
nurzhanuly <nurzhanuly@kolesa.kz>
2022-05-21 19:23:36 +0600

    EME - adds version command

---
+++ cmd/version.go
@@ -0,0 +1,25 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Get a version of the tool",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("version called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
