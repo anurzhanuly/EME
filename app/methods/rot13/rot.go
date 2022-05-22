ff239031ec5ad126c0c58dda47fe9e00e4d23b54
nurzhanuly <nurzhanuly@kolesa.kz>
2022-05-23 01:02:15 +0600

    EME - skeleton for ROT

--- app/methods/rot13/rot.go
+++ app/methods/rot13/rot.go
@@ -1,11 +1,19 @@
 package rot13
 
+import headerUtil "ABA/EME/app/methods/utils/header"
+
 type Detector struct {
 	Filepath string
 	Result   bool
 }
 
 func (d *Detector) Detect() bool {
+	header := headerUtil.GetFileHeader(d.Filepath)
+
+	return isROTed(header)
+}
+
+func isROTed(header [2]byte) bool {
 
 	return false
 }
