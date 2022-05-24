af23c704874647b58568dadf57ece2c86a0c60b9
nurzhanuly <nurzhanuly@kolesa.kz>
2022-05-24 20:58:58 +0600

    EME - adds basic unit-test for rot13

--- app/methods/rot13/rot_test.go
+++ app/methods/rot13/rot_test.go
@@ -1,1 +1,20 @@
 package rot13
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestRot13(t *testing.T) {
+	input := []byte("Lbh penpxrq gur pbqr!")
+	output := []byte("You cracked the code!")
+	result := make([]byte, len(input))
+
+	for index, value := range input {
+		result[index] = Rot13(value, 13)
+	}
+
+	if bytes.Contains(result, output) {
+		t.Log("KRASAVA! ROT PASHET")
+	}
+}
